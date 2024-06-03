daebf599ef50873cc0fe43208844bb547f59cb45
孟凡荣 <mfr01606587@alibaba-inc.com>
2024-06-03 18:24:03 +0800

    feat: uplod

--- src/router/postTest.js
+++ src/router/postTest.js
@@ -1,17 +1,43 @@
 const router = require("koa-router")(); // 注意：不是koa-route
 const koaBody = require("koa-body");
+const koaForm = require("formidable-upload-koa");
+const path = require("path");
 
 let personInfo = {};
 
 router.post("/set/persion/info", koaBody(), async (ctx, next) => {
-  let getData = ctx.request.body;
-  console.log('getData', getData);
-  personInfo = getData;
-  ctx.body = getData;
+  let body = ctx.request.body;
+  let search = ctx.request.search;
+  let query = ctx.request.query;
+
+  console.log("body", body);
+  console.log("search", search);
+  console.log("query", query);
+  personInfo = body;
+  ctx.body = body;
 });
 
 router.get("/get/persion/info", async (ctx, next) => {
   ctx.body = personInfo;
 });
+
+router.post(
+  "/upload/persion",
+  koaForm({
+    uploadDir: path.join(__dirname, "../public/image"), // 文件存放的位置
+    keepExtensions: true, // 包含扩展名
+    maxFileSize: 1024 * 1024 * 100, // 大小为 1m
+  }),
+  async (ctx, next) => {
+    const file = ctx.req.files["file"];
+    const { size, name, path } = file;
+    // console.log('sizesizesizesize',file[0]);
+    ctx.body = {
+      size,
+      name,
+      url: `http://127.0.0.1:3033/image/${path.split("/").slice(-1)[0]}`,
+    };
+  }
+);
 
 exports.setPersionInfo = router;
